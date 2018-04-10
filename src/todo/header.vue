fd2193b865976ad1bce53f8fd0c87d050c695434
willdonner <765019300@qq.com>
2018-04-10 23:44:10 +0800

    add vue

--- src/todo/header.vue
+++ src/todo/header.vue
@@ -1,17 +1,19 @@
 <template>
-  <header class="main-header">
-      <h1>willdonner_Todo</h1>
-  </header>
+    <header class="main-header">
+        <h1>TodoAPP</h1>
+    </header>
 </template>
-<style>
-.main-header{
-    text-align: center;
-}
-h1{
-        font-size: 70px;
-        color: green;
-        font-weight: 70%; 
-        margin: 20px;
-    }
-</style>
 
+<style lang="stylus" scoped>
+    .main-header {
+        text-align center
+        h1 {
+            font-size 100px
+            color rgba(175, 47, 47, 0.4)
+            margin 20px
+        }
+
+    }
+
+
+</style>
